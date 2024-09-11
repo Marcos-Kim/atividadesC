9000332a7a6fd41524bcb0c0262f647b654543ae
Marcos Kim <marcos.skim@senacsp.edu.br>
2024-09-11 14:14:58 +0000

    Exercício de nota média para aprovação ou reprovação.

---
+++ nota_media.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    float nota1, nota2, nota3, nota4, media;
+    printf ("Qual a primeira nota?\n");
+    scanf ("%f" , &nota1);
+
+    printf ("Qual a primeira nota?\n");
+    scanf ("%f" , &nota2);
+
+    printf ("Qual a primeira nota?\n");
+    scanf ("%f" , &nota3);
+
+    printf ("Qual a primeira nota?\n");
+    scanf ("%f" , &nota4);
+
+    media = (nota1 + nota2 + nota3 + nota4) /4;
+
+    if (media >= 6){
+        printf("Aprovado(a)");
+    }
+    else{
+        printf("Reprovado(a)");
+    }
+    return 0;
+}
